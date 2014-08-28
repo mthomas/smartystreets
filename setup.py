85f0a51845090410d2cd00bcb9fd3f505da45a88
mthomas <mbthomas@gmail.com>
2014-08-28 14:33:05 -0400

    rename to smartystreets2

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'smartystreets',
-  packages = ['smartystreets'], # this must be the same as the name above
-  version = '0.1',
+  name = 'smartystreets2',
+  packages = ['smartystreets2'], # this must be the same as the name above
+  version = '0.2',
   description = 'An API for smartystreets',
   author = 'Michael Thomas',
   author_email = 'peterldowns@gmail.com',
   url = 'https://github.com/mthomas/smartystreets', # use the URL to the github repo
-  download_url = 'https://github.com/mthomas/smartystreets/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/mthomas/smartystreets/tarball/0.2', # I'll explain this in a second
   keywords = ['api', 'address'], # arbitrary keywords
   classifiers = [],
 )
